11f34b3f6a7629e2047208070d6e11ff6a140da9
MiguelAguilera <miguel@maguilera.net>
2013-12-10 11:34:44 +0100

    pre-processing of stats data

--- run_connectivity.sh
+++ run_connectivity.sh
@@ -1,4 +1,12 @@
 
+
+effconnectivity/data-forum
+effconnectivity/data-stats
+cd effconnectivity/data/
+ls *.txt > files
+cd ..
+cd ..
+
 javac effconnectivity/*.java
 
 DATE=`date +%Y-%m-%d`
@@ -7,4 +15,4 @@ java effconnectivity.Connectivity 1 > results/connectivity/x-corr/$DATE.csv
 java effconnectivity.Connectivity 2 8 2 > results/connectivity/mutual-information/$DATE.csv
 java effconnectivity.Connectivity 3 8 2 2 > results/connectivity/transfer-entropy/$DATE.csv
 
-rm data/forum/*.txt
+rm effconnectivity/data/*
